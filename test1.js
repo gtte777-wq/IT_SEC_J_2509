423df5cfa17264efbb325f2663a0f38290d63f20
gtte777-wq <gtte777@gmail.com>
2025-09-17 11:02:08 +0900

    .

--- test1.js
+++ test1.js
@@ -1,2 +1,32 @@
-console.assert(false);
+//console.assert(false);
+let a = 1;
+console.log(a);
+let b=2;
+let c=a+b;
+console.log(`a+b=` , c);
+console.log(`a(${a}) + b(${b}) = ${c}`);
+let d;
+console.log(`a:${a}`);
+console.log(`b:${b}`);
+console.log(`c:${c}`);
+a = a + 3;
+console.log(`a:${a}`);
+
+a=1;
+a++;
+console.log(`a:${a}`);
+
+const con1 = 3;
+
+a = 1;
+b = "2";
+c = a + b;
+console.log(`c:${c}`);
+
+let num1 = 3
+let num2 = 2
+let result = num1/num2;
+console.log(`num1:${num1}`);
+console.log(`num2:${num2}`);
+console.log(`result:${result}`);
 
